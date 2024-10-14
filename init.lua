4d6f8c93c118b26436d73e7ec9dcfbb749f630eb
Monish Thirukumaran <monish.thir@gmail.com>
2024-10-14 17:21:01 +0100

    feat: add smart-split compatiable tmux pane option

--- init.lua
+++ init.lua
@@ -1,2 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.api.nvim_create_augroup("TmuxVariables", { clear = true })
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "TmuxVariables",
+  callback = function()
+    vim.fn.system('tmux set-option -p @pane-is-vim 1')
+  end
+})
+vim.api.nvim_create_autocmd("VimLeave", {
+  group = "TmuxVariables",
+  callback = function()
+    vim.fn.system('tmux set-option -p @pane-is-vim 0')
+  end
+})
