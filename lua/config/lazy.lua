f44f19fb49800245d0c510e0cf98752044a64372
Monish Thirukumaran <monish.thir@gmail.com>
2024-12-31 15:37:14 +0000

    feat: add copilot chat

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.gleam" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.copilot-chat" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
